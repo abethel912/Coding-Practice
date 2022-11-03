6341facbbf92aa63762507fc1c78981332b86d7e
abethel912 <abethel180@gmail.com>
2022-11-03 18:51:56 -0400

    Solved string to number problem

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-// Let's start with a conversion from kilometers to miles. The function should include the input in kilometers and return the answer in miles.
+// // Let's start with a conversion from kilometers to miles. The function should include the input in kilometers and return the answer in miles.
 
 
-function kmToMiles(kilometer) {
-  return kilometer * 0.62137
-}
-//
-console.log(kmToMiles(3))
+// function kmToMiles(kilometer) {
+//   return kilometer * 0.62137
+// }
+// //
+// console.log(kmToMiles(3))
 
-// Calculate the sum of numbers within an array
+// // Calculate the sum of numbers within an array
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8];
-let total = 0;
+// const array = [1, 2, 3, 4, 5, 6, 7, 8];
+// let total = 0;
 
-for (let i = 0; i < array.length; i++) {
-  total += array[i];
-}
+// for (let i = 0; i < array.length; i++) {
+//   total += array[i];
+// }
 
-console.log(total)
+// console.log(total)
+
+// function polygonTotal(length, width) {
+//   if (length === width) {
+//     return length * width
+//   }
+//   if (length !== width) {
+//     return 2 * (length + width)
+//   }
+// }
+
+// console.log(polygonTotal(5, 4))
+
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+
+
+
