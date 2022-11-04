21639f92a63ab6bb71777e5ac44198d4880f4974
abethel912 <abethel180@gmail.com>
2022-11-03 22:34:10 -0400

    stringToNumber and past functions solved

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-// // Let's start with a conversion from kilometers to miles. The function should include the input in kilometers and return the answer in miles.
+// Let's start with a conversion from kilometers to miles. The function should include the input in kilometers and return the answer in miles.
 
-
-// function kmToMiles(kilometer) {
-//   return kilometer * 0.62137
-// }
-// //
-// console.log(kmToMiles(3))
+function kmToMiles(kilometer) {
+  return kilometer * 0.62137
+}
+//
+console.log(kmToMiles(3))
 
 // // Calculate the sum of numbers within an array
 
-// const array = [1, 2, 3, 4, 5, 6, 7, 8];
-// let total = 0;
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+let total = 0;
 
-// for (let i = 0; i < array.length; i++) {
-//   total += array[i];
-// }
+for (let i = 0; i < array.length; i++) {
+  total += array[i];
+}
 
-// console.log(total)
+console.log(total)
 
-// function polygonTotal(length, width) {
-//   if (length === width) {
-//     return length * width
-//   }
-//   if (length !== width) {
-//     return 2 * (length + width)
-//   }
-// }
+// Write a function that calculates the length and width of a polygon.
 
-// console.log(polygonTotal(5, 4))
+function polygonTotal(length, width) {
+  if (length === width) {
+    return length * width
+  }
+  if (length !== width) {
+    return 2 * (length + width)
+  }
+}
+
+console.log(polygonTotal(5, 4))
 
 // We need a function that can transform a string into a number. What ways of achieving this do you know?
 
+const stringToNumber = function (str) {
+  return Number(str)
+}
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+function past(h, m, s) {
+  if (0 <= h <= 23, 0 <= m <= 59, 0 <= s <= 59) 
+    return (h * 1000 * 60 * 60) + (m * 1000 * 60) + (s * 1000)  
+}
+  
 
 
 
