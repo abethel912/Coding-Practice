86620131ed4f20c9009a6c0e510a2c1143bff6c3
abethel912 <abethel180@gmail.com>
2022-11-03 22:45:11 -0400

    Solved repeatStr function

--- script.js
+++ script.js
@@ -42,7 +42,12 @@ function past(h, m, s) {
   if (0 <= h <= 23, 0 <= m <= 59, 0 <= s <= 59) 
     return (h * 1000 * 60 * 60) + (m * 1000 * 60) + (s * 1000)  
 }
-  
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+ 
+function repeatStr(n, s) {
+  return s.repeat(n)
+}
 
 
 
